44f7067816fdc3ffc6aa9f940a1e85c894d63aba
kuzma17 <v.kuzma@mail.ru>
2017-10-28 19:52:23 +0300

    2

--- form.py
+++ form.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import cgi
 import os
-import xml.etree.cElementTree as CE
+import xml.etree.cElementTree as ET
 #import xml.dom.minidom as MN
 #from xml.etree import cElementTree
 
@@ -13,17 +13,44 @@ text2 = form.getfirst("param1", "не задано")
 text3 = "123"
 
 print("Contenr-type:text/html\n\n")
-
+print("<html>")
+print("<head>")
+print("</head>")
+print("<body>")
 
 print("Hello CGI \n")
 print(text1 + "\n")
 print(text2 + "\n")
 
-
-#with open('/var/www/html/parse_erc/xmlfile.xml', 'rt') as f:
 fileXML = '/var/www/html/parse_erc/erc_selected_vendors_20171017_04h25m.xml'
-tree = CE.parse(fileXML)
 
-for node in tree.iter(content):
-    #print(node[0][0][0])
-    print(node.findtext('./gname/Value'))
+elem = ET.parse(fileXML)
+root = elem.getroot()
+print(root)
+print(len(root))
+
+new = ET.Element('new_xml')
+
+for vendor in root.findall('vendor'):
+    goods = vendor.findall('goods')
+    for good in goods:
+        #print(good[2].text + ' code = ' + good[3].text + '<br>')
+
+        item = ET.SubElement(new, 'item')
+        title = ET.SubElement(item, 'title')
+        title.text = good[2].text
+        code = ET.SubElement(item, 'code')
+        code.text = good[3].text
+        category = ET.SubElement(item, 'category')
+        category.text = good[1].text
+        price = ET.SubElement(item, 'price')
+        price.text = str(round(float(good[7].text) * 1.025, 2))
+        price_r = ET.SubElement(item, 'price_r')
+        price_r.text = str(round(float(good[5].text) * 1.075, 2))
+
+ET.ElementTree(new).write('/var/www/html/parse_erc/new.xml')
+
+print('=== New XML Created ===')
+
+print("</body>")
+print("</html>")
--- parse.py
+++ parse.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
-import xml.etree.cElementTree as etree
+import xml.etree.cElementTree as ET
 
-fileXML = '/var/www/parse_erc/erc_selected_vendors_20171017_04h25m.xml'
-tree = etree.parse(fileXML)
-root = tree.getroot()
+fileXML = '/var/www/html/parse_erc/erc_selected_vendors_20171017_04h25m.xml'
+
+print("Contenr-type:text/html\n\n")
+
+elem = ET.parse(fileXML)
+root = elem.getroot()
 print(root)
 print(len(root))
+
+new = ET.Element('new_xml')
 
 for vendor in root.findall('vendor'):
     goods = vendor.findall('goods')
     for good in goods:
-        print(good[2].text + ' code = ' + good[3].text)
+        print(good[2].text + ' code = ' + good[3].text + '<br>')
+
+        price_text = good[5].text + " === " + str(round(float(good[5].text) * 1.134, 2))
+
+        item = ET.SubElement(new, 'item')
+        title = ET.SubElement(item, 'title')
+        title.text = good[2].text
+        code = ET.SubElement(item, 'code')
+        code.text = good[3].text
+        category = ET.SubElement(item, 'category')
+        category.text = good[1].text
+        price = ET.SubElement(item, 'price')
+        price.text = price_text
+
+ET.ElementTree(new).write('/var/www/html/parse_erc/new.xml')
+
+
+print('End parse.')

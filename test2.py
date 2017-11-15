293395e60ef88ddd860abd1312b3a1098bcdf6fd
kuzma17 <v.kuzma@mail.ru>
2017-11-15 21:22:46 +0200

    ajax work !!!

---
+++ test2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import MySQLdb
+import cgi
+import os
+import xml.etree.cElementTree as ET
+import time
+
+db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="170270", db="parse_erc", charset='utf8', use_unicode=False)
+cursor = db.cursor()
+
+print("Content-type:text/html\n\n")
+print("""<html>
+    <head>
+    </head>
+    <body>""")
+
+sql = "SELECT name FROM erc_sub_categories WHERE id = 2"
+cursor.execute(sql)
+name = cursor.fetchone()[0].decode()
+print(name)
+print('<br>')
+
+print("""</body
+</html>""")
+
+
+

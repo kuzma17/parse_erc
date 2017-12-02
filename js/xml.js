df78b75a46d2018658b253005589b8748853ebfd
kuzma <kuzma@maill.ru>
2017-12-02 17:40:54 +0200

    normalize code jquery

--- js/xml.js
+++ js/xml.js
@@ -1,21 +1,21 @@
 /**
  * Created by kuzma on 30.11.17.
  */
-$(document).ready(function() {
+$(document).ready(function () {
     $('#preloader').show();
     $.ajax({
         type: 'POST',
         url: 'currency.py',
-        success: function(data) {
+        success: function (data) {
             $('#preloader').hide();
             $("#currency").val(data);
         }
     });
 
     var filesExt = ['xml', 'XML', 'Xml'];
-    $('#upload-file-btn').click(function() {
+    $('#upload-file-btn').click(function () {
         var parts = $('#file').val().split('.');
-        if(filesExt.join().search(parts[parts.length - 1]) == -1){
+        if (filesExt.join().search(parts[parts.length - 1]) == -1) {
             alert('Не корректный исходный файл!');
             return false;
         }
@@ -31,13 +31,13 @@ $(document).ready(function() {
             cache: false,
             processData: false,
             async: false,
-            success: function(data) {
+            success: function (data) {
                 onResponse(data)
             }
         });
     });
 
-    $('#info').on("click", "#edit-param", function() {
+    $('#info').on("click", "#edit-param", function () {
         $('#preloader').show();
         var form_data = new FormData($('#category_save')[0]);
         $('#info_myForm').hide();
@@ -50,13 +50,13 @@ $(document).ready(function() {
             cache: false,
             processData: false,
             async: false,
-            success: function(data) {
+            success: function (data) {
                 onResponse(data)
             }
         });
     });
 
-    $('#info').on("click", "#create_xml", function() {
+    $('#info').on("click", "#create_xml", function () {
         $('#preloader').show();
         var form_data = new FormData($('#category_save')[0]);
         $('#info_myForm').hide();
@@ -69,36 +69,38 @@ $(document).ready(function() {
             cache: false,
             processData: false,
             async: false,
-            success: function(data) {
+            success: function (data) {
                 //onResponse(data)
                 $('#preloader').hide();
                 $("#info").html(data);
                 //$("#info").show(300);
-                $("#info").slideDown(400);;
+                $("#info").slideDown(400);
+                ;
             }
         });
     });
 
-    function onResponse(data){
+    function onResponse(data) {
         $('#preloader').hide();
         $("#info").html(data);
         //$("#info").show(300);
-        $("#info").slideDown(1000);;
+        $("#info").slideDown(1000);
+        ;
     }
 
-    $('#info').on("mouseover", ".code_prom", function(){
+    $('#info').on("mouseover", ".code_prom", function () {
         $(this).css({marginTop: '1px'});
         $(this).children("span").css({fontSize: '12px'});
     });
-    $('#info').on("mouseout", ".code_prom", function(){
+    $('#info').on("mouseout", ".code_prom", function () {
         $(this).css({marginTop: '-1px'});
         $(this).children("span").css({fontSize: '8px'});
     });
-    $('#info').on("click", "#all_checkbox", function(){
-        if($(this).prop("checked")){
+    $('#info').on("click", "#all_checkbox", function () {
+        if ($(this).prop("checked")) {
             $('#all_checkbox_text').html('Снять все');
             $('.xml_check').prop("checked", true);
-        }else{
+        } else {
             $('#all_checkbox_text').html('Выбрать все');
             $('.xml_check').prop("checked", false);
         }
